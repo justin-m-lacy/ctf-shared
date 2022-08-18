8047e1a05640ddc8ab5f351d3535ec8cddce4f61
Justin Lacy <justin@launchbadge.com>
2022-08-18 02:49:03 -0700

    export flatbuffers

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
+
+export { MessageType } from 'src/flatbuffers/ctf/message-type';
+export { FlagDropped } from 'src/flatbuffers/ctf/flag-dropped';
+export { FlagReturned } from 'src/flatbuffers/ctf/flag-returned';
+export { FlagTaken } from 'src/flatbuffers/ctf/flag-taken';
+export { TeamWon } from 'src/flatbuffers/ctf/team-won';
+export { TeamScored } from 'src/flatbuffers/ctf/team-scored';
+export { PlayerMove } from 'src/flatbuffers/ctf/player-move';
+export { PlayerChargeFire } from 'src/flatbuffers/ctf/player-charge-fire';
+export { PlayerCancelFire } from 'src/flatbuffers/ctf/player-cancel-fire';
+export { PlayerFire } from 'src/flatbuffers/ctf/player-fire';
+export { PlayerKilled } from 'src/flatbuffers/ctf/player-killed';
+export { PlayerRespawn } from 'src/flatbuffers/ctf/player-respawn';
+
 export * from './src/messages/encode';
 export * from './src/messages/decode';
