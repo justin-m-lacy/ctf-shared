cb7e71b5f5e04ed8ce6c5e83cc3018fca842f5a8
Justin Lacy <justin@launchbadge.com>
2022-08-17 20:12:30 -0700

    export encoder and decoder objects

--- src/messages/decode.ts
+++ src/messages/decode.ts
@@ -14,47 +14,52 @@ import { TeamScored } from '../flatbuffers/ctf/team-scored';
 import { TeamWon } from '../flatbuffers/ctf/team-won';
 import { CtfEvent, PlayerEvent, GameEvent } from './types';
 
-export const parseMessage = function (raw: Uint8Array): CtfEvent | null {
+export const Decoder = {
 
-    let buf = new flatbuffers.ByteBuffer(raw);
-    let msg = CtfMessage.getRootAsCtfMessage(buf);
 
-    return parseContent(msg);
+    decodeMessage(raw: Uint8Array): CtfEvent | null {
 
-}
+        let buf = new flatbuffers.ByteBuffer(raw);
+        let msg = CtfMessage.getRootAsCtfMessage(buf);
 
-export const parseContent = (msg: CtfMessage): CtfEvent | null => {
+        return this.decodeContent(msg);
 
-    console.log(`content type: ${msg.contentType()}`);
+    },
 
-    switch (msg.contentType()) {
-        case MessageContent.PlayerMove:
-            return msg.content(new PlayerMove()) as PlayerMove;
-        case MessageContent.PlayerChargeFire:
-            return msg.content(new PlayerCancelFire()) as PlayerChargeFire;
-        case MessageContent.PlayerCancelFire:
-            return msg.content(new PlayerCancelFire()) as PlayerCancelFire;
-        case MessageContent.PlayerFire:
-            return msg.content(new PlayerFire()) as PlayerFire;
+    decodeContent(msg: CtfMessage): CtfEvent | null {
 
-        case MessageContent.PlayerKilled:
-            return msg.content(new PlayerKilled()) as PlayerKilled;
-        case MessageContent.PlayerRespawn:
-            return msg.content(new PlayerRespawn()) as PlayerRespawn;
-        case MessageContent.FlagTaken:
-            return msg.content(new FlagTaken()) as FlagTaken;
-        case MessageContent.FlagDropped:
-            return msg.content(new FlagDropped()) as FlagDropped;
-        case MessageContent.FlagReturned:
-            return msg.content(new FlagReturned()) as FlagReturned;
+        console.log(`content type: ${msg.contentType()}`);
 
-        case MessageContent.TeamScored:
-            return msg.content(new TeamScored()) as TeamScored;
-        case MessageContent.TeamWon:
-            return msg.content(new TeamWon()) as TeamWon;
-        default:
-            console.log(`no message type parsed.`);
-            return null;
+        switch (msg.contentType()) {
+            case MessageContent.PlayerMove:
+                return msg.content(new PlayerMove()) as PlayerMove;
+            case MessageContent.PlayerChargeFire:
+                return msg.content(new PlayerCancelFire()) as PlayerChargeFire;
+            case MessageContent.PlayerCancelFire:
+                return msg.content(new PlayerCancelFire()) as PlayerCancelFire;
+            case MessageContent.PlayerFire:
+                return msg.content(new PlayerFire()) as PlayerFire;
 
+            case MessageContent.PlayerKilled:
+                return msg.content(new PlayerKilled()) as PlayerKilled;
+            case MessageContent.PlayerRespawn:
+                return msg.content(new PlayerRespawn()) as PlayerRespawn;
+            case MessageContent.FlagTaken:
+                return msg.content(new FlagTaken()) as FlagTaken;
+            case MessageContent.FlagDropped:
+                return msg.content(new FlagDropped()) as FlagDropped;
+            case MessageContent.FlagReturned:
+                return msg.content(new FlagReturned()) as FlagReturned;
+
+            case MessageContent.TeamScored:
+                return msg.content(new TeamScored()) as TeamScored;
+            case MessageContent.TeamWon:
+                return msg.content(new TeamWon()) as TeamWon;
+            default:
+                console.log(`no message type parsed.`);
+                return null;
+
+        }
     }
+
 }
--- src/messages/encode.ts
+++ src/messages/encode.ts
@@ -16,182 +16,183 @@ import { CtfMessage } from '../flatbuffers/ctf/ctf-message';
 import { MessageContent } from '../flatbuffers/ctf/message-content';
 import { maxIndex } from '../util/array';
 
+export const Encoder = {
+    makeFlagReturned(userid: string, at: Point, teamFlag: number, builder?: flatbuffers.Builder) {
 
-export const makeFlagReturned = (userid: string, at: Point, teamFlag: number, builder?: flatbuffers.Builder) => {
+        builder = builder ?? new flatbuffers.Builder(1024);
 
-    builder = builder ?? new flatbuffers.Builder(1024);
+        const byIndex = builder.createString(userid);
 
-    const byIndex = builder.createString(userid);
+        FlagReturned.startFlagReturned(builder);
+        FlagReturned.addBy(builder, byIndex);
+        FlagReturned.addAt(builder, this.makeVec2(at, builder));
+        FlagReturned.addTeamFlag(builder, teamFlag)
 
-    FlagReturned.startFlagReturned(builder);
-    FlagReturned.addBy(builder, byIndex);
-    FlagReturned.addAt(builder, makeVec2(at, builder));
-    FlagReturned.addTeamFlag(builder, teamFlag)
+        return this.makeMessage(FlagReturned.endFlagReturned(builder), MessageContent.FlagReturned, builder);
 
-    return makeMessage(FlagReturned.endFlagReturned(builder), MessageContent.FlagReturned, builder);
+    },
 
-}
 
+    makeFlagTaken(userid: string, at: Point, flagTeam: number, builder?: flatbuffers.Builder) {
 
-export const makeFlagTaken = (userid: string, at: Point, flagTeam: number, builder?: flatbuffers.Builder) => {
+        builder = builder ?? new flatbuffers.Builder(1024);
 
-    builder = builder ?? new flatbuffers.Builder(1024);
+        const whoIndex = builder.createString(userid);
 
-    const whoIndex = builder.createString(userid);
+        FlagTaken.startFlagTaken(builder);
+        FlagTaken.addWho(builder, whoIndex);
+        FlagTaken.addAt(builder, this.makeVec2(at, builder));
+        FlagTaken.addFlagTeam(builder, flagTeam);
 
-    FlagTaken.startFlagTaken(builder);
-    FlagTaken.addWho(builder, whoIndex);
-    FlagTaken.addAt(builder, makeVec2(at, builder));
-    FlagTaken.addFlagTeam(builder, flagTeam);
+        return this.makeMessage(FlagTaken.endFlagTaken(builder), MessageContent.FlagTaken, builder);
 
-    return makeMessage(FlagTaken.endFlagTaken(builder), MessageContent.FlagTaken, builder);
+    },
 
-}
+    makeFlagDropped(userid: string, at: Point, flagTeam: number, builder?: flatbuffers.Builder) {
 
-export const makeFlagDropped = (userid: string, at: Point, flagTeam: number, builder?: flatbuffers.Builder) => {
+        builder = builder ?? new flatbuffers.Builder(1024);
 
-    builder = builder ?? new flatbuffers.Builder(1024);
+        const whoIndex = builder.createString(userid);
 
-    const whoIndex = builder.createString(userid);
+        FlagDropped.startFlagDropped(builder);
+        FlagDropped.addBy(builder, whoIndex);
+        FlagDropped.addAt(builder, this.makeVec2(at, builder));
+        FlagDropped.addFlagTeam(builder, flagTeam);
 
-    FlagDropped.startFlagDropped(builder);
-    FlagDropped.addBy(builder, whoIndex);
-    FlagDropped.addAt(builder, makeVec2(at, builder));
-    FlagDropped.addFlagTeam(builder, flagTeam);
+        return this.makeMessage(FlagDropped.endFlagDropped(builder), MessageContent.FlagDropped, builder);
 
-    return makeMessage(FlagDropped.endFlagDropped(builder), MessageContent.FlagDropped, builder);
+    },
 
-}
+    makeTeamWon(scores: number[], builder?: flatbuffers.Builder) {
 
-export const makeTeamWon = (scores: number[], builder?: flatbuffers.Builder) => {
+        builder = builder ?? new flatbuffers.Builder(1024);
 
-    builder = builder ?? new flatbuffers.Builder(1024);
+        const winner = maxIndex(scores);
+        const scoresIndex = TeamWon.createScoresVector(builder, scores);
 
-    const winner = maxIndex(scores);
-    const scoresIndex = TeamWon.createScoresVector(builder, scores);
+        TeamWon.startTeamWon(builder);
+        TeamWon.addWinner(builder, winner);
+        TeamWon.addScores(builder, scoresIndex);
 
-    TeamWon.startTeamWon(builder);
-    TeamWon.addWinner(builder, winner);
-    TeamWon.addScores(builder, scoresIndex);
+        return this.makeMessage(TeamWon.endTeamWon(builder), MessageContent.TeamWon, builder);
 
-    return makeMessage(TeamWon.endTeamWon(builder), MessageContent.TeamWon, builder);
+    },
 
-}
+    makeTeamScored(userid: string, scoringTeam: number, flagTeam: number, scores: number[], builder?: flatbuffers.Builder) {
 
-export const makeTeamScored = (userid: string, scoringTeam: number, flagTeam: number, scores: number[], builder?: flatbuffers.Builder) => {
+        builder = builder ?? new flatbuffers.Builder(1024);
 
-    builder = builder ?? new flatbuffers.Builder(1024);
+        const whoIndex = builder.createString(userid);
+        const scoresIndex = TeamWon.createScoresVector(builder, scores);
 
-    const whoIndex = builder.createString(userid);
-    const scoresIndex = TeamWon.createScoresVector(builder, scores);
+        TeamScored.startTeamScored(builder);
+        TeamScored.addWho(builder, whoIndex);
+        TeamScored.addScores(builder, scoresIndex);
+        TeamScored.addTeam(builder, scoringTeam)
+        TeamScored.addFlagTeam(builder, flagTeam);
 
-    TeamScored.startTeamScored(builder);
-    TeamScored.addWho(builder, whoIndex);
-    TeamScored.addScores(builder, scoresIndex);
-    TeamScored.addTeam(builder, scoringTeam)
-    TeamScored.addFlagTeam(builder, flagTeam);
+        return this.makeMessage(TeamScored.endTeamScored(builder), MessageContent.TeamScored, builder);
 
-    return makeMessage(TeamScored.endTeamScored(builder), MessageContent.TeamScored, builder);
+    },
 
-}
+    makePlayerChargeFire(userid: string, at: Point, builder?: flatbuffers.Builder) {
 
-export const makePlayerChargeFire = (userid: string, at: Point, builder?: flatbuffers.Builder) => {
+        builder = builder ?? new flatbuffers.Builder(1024);
 
-    builder = builder ?? new flatbuffers.Builder(1024);
+        const whoIndex = builder.createString(userid);
 
-    const whoIndex = builder.createString(userid);
+        PlayerChargeFire.startPlayerChargeFire(builder);
+        PlayerChargeFire.addWho(builder, whoIndex);
+        PlayerChargeFire.addAt(builder, this.makeVec2(at, builder));
 
-    PlayerChargeFire.startPlayerChargeFire(builder);
-    PlayerChargeFire.addWho(builder, whoIndex);
-    PlayerChargeFire.addAt(builder, makeVec2(at, builder));
+        return this.makeMessage(PlayerChargeFire.endPlayerChargeFire(builder), MessageContent.PlayerChargeFire, builder);
 
-    return makeMessage(PlayerChargeFire.endPlayerChargeFire(builder), MessageContent.PlayerChargeFire, builder);
+    },
+    makePlayerCancelFire(userid: string, builder?: flatbuffers.Builder) {
 
-}
-export const makePlayerCancelFire = (userid: string, builder?: flatbuffers.Builder) => {
+        builder = builder ?? new flatbuffers.Builder(1024);
 
-    builder = builder ?? new flatbuffers.Builder(1024);
+        const whoIndex = builder.createString(userid);
 
-    const whoIndex = builder.createString(userid);
+        PlayerCancelFire.startPlayerCancelFire(builder);
+        PlayerCancelFire.addWho(builder, whoIndex);
 
-    PlayerCancelFire.startPlayerCancelFire(builder);
-    PlayerCancelFire.addWho(builder, whoIndex);
+        return this.makeMessage(PlayerCancelFire.endPlayerCancelFire(builder), MessageContent.PlayerCancelFire, builder);
 
-    return makeMessage(PlayerCancelFire.endPlayerCancelFire(builder), MessageContent.PlayerCancelFire, builder);
+    },
 
-}
+    makePlayerKilled(userid: string, by: string, at: Point, builder?: flatbuffers.Builder) {
 
-export const makePlayerKilled = (userid: string, by: string, at: Point, builder?: flatbuffers.Builder) => {
+        builder = builder ?? new flatbuffers.Builder(1024);
 
-    builder = builder ?? new flatbuffers.Builder(1024);
+        const whoIndex = builder.createString(userid);
+        const byIndex = builder.createString(by);
 
-    const whoIndex = builder.createString(userid);
-    const byIndex = builder.createString(by);
+        PlayerKilled.startPlayerKilled(builder);
+        PlayerKilled.addWho(builder, whoIndex);
+        PlayerKilled.addBy(builder, byIndex);
+        PlayerKilled.addAt(builder, this.makeVec2(at, builder));
 
-    PlayerKilled.startPlayerKilled(builder);
-    PlayerKilled.addWho(builder, whoIndex);
-    PlayerKilled.addBy(builder, byIndex);
-    PlayerKilled.addAt(builder, makeVec2(at, builder));
+        return this.makeMessage(PlayerKilled.endPlayerKilled(builder), MessageContent.PlayerKilled, builder);
 
-    return makeMessage(PlayerKilled.endPlayerKilled(builder), MessageContent.PlayerKilled, builder);
+    },
 
-}
+    makePlayerFire(userid: string, at: Point, orientation: Point, strength: number, builder?: flatbuffers.Builder) {
 
-export const makePlayerFire = (userid: string, at: Point, orientation: Point, strength: number, builder?: flatbuffers.Builder) => {
+        builder = builder ?? new flatbuffers.Builder(1024);
 
-    builder = builder ?? new flatbuffers.Builder(1024);
+        const whoIndex = builder.createString(userid);
 
-    const whoIndex = builder.createString(userid);
+        PlayerFire.startPlayerFire(builder);
+        PlayerFire.addWho(builder, whoIndex);
+        PlayerFire.addAt(builder, this.makeVec2(at, builder));
+        PlayerFire.addOrientation(builder, this.makeVec2(orientation, builder));
+        PlayerFire.addStrength(builder, strength);
 
-    PlayerFire.startPlayerFire(builder);
-    PlayerFire.addWho(builder, whoIndex);
-    PlayerFire.addAt(builder, makeVec2(at, builder));
-    PlayerFire.addOrientation(builder, makeVec2(orientation, builder));
-    PlayerFire.addStrength(builder, strength);
+        return this.makeMessage(PlayerFire.endPlayerFire(builder), MessageContent.PlayerFire, builder);
 
-    return makeMessage(PlayerFire.endPlayerFire(builder), MessageContent.PlayerFire, builder);
+    },
 
-}
+    makePlayerMove(userid: string, to: Point, builder?: flatbuffers.Builder) {
 
-export const makePlayerMove = (userid: string, to: Point, builder?: flatbuffers.Builder) => {
+        builder = builder ?? new flatbuffers.Builder(1024);
 
-    builder = builder ?? new flatbuffers.Builder(1024);
+        const whoIndex = builder.createString(userid);
 
-    const whoIndex = builder.createString(userid);
+        PlayerMove.startPlayerMove(builder);
+        PlayerMove.addWho(builder, whoIndex);
+        PlayerMove.addTo(builder, this.makeVec2(to, builder));
 
-    PlayerMove.startPlayerMove(builder);
-    PlayerMove.addWho(builder, whoIndex);
-    PlayerMove.addTo(builder, makeVec2(to, builder));
+        return this.makeMessage(PlayerMove.endPlayerMove(builder), MessageContent.PlayerMove, builder);
 
-    return makeMessage(PlayerMove.endPlayerMove(builder), MessageContent.PlayerMove, builder);
+    },
 
-}
+    makePlayerRespawn(userid: string, at: Point, builder?: flatbuffers.Builder) {
 
-export const makePlayerRespawn = (userid: string, at: Point, builder?: flatbuffers.Builder) => {
+        builder = builder ?? new flatbuffers.Builder(1024);
 
-    builder = builder ?? new flatbuffers.Builder(1024);
+        const whoIndex = builder.createString(userid);
 
-    const whoIndex = builder.createString(userid);
+        PlayerRespawn.startPlayerRespawn(builder);
+        PlayerRespawn.addWho(builder, whoIndex);
+        PlayerRespawn.addAt(builder, this.makeVec2(at, builder));
 
-    PlayerRespawn.startPlayerRespawn(builder);
-    PlayerRespawn.addWho(builder, whoIndex);
-    PlayerRespawn.addAt(builder, makeVec2(at, builder));
+        return this.makeMessage(PlayerRespawn.endPlayerRespawn(builder), MessageContent.PlayerRespawn, builder);
 
-    return makeMessage(PlayerRespawn.endPlayerRespawn(builder), MessageContent.PlayerRespawn, builder);
+    },
+    makeMessage(contentInd: number, contentType: number, builder: flatbuffers.Builder) {
 
-}
+        const offset = CtfMessage.createCtfMessage(builder, contentType, contentInd, Date.now());
+        builder.finish(offset);
 
-export const makeMessage = (contentInd: number, contentType: number, builder: flatbuffers.Builder) => {
+        return builder.asUint8Array();
 
-    const offset = CtfMessage.createCtfMessage(builder, contentType, contentInd, Date.now());
-    builder.finish(offset);
+    },
 
-    return builder.asUint8Array();
+    makeVec2(to: Point, builder: flatbuffers.Builder) {
 
-}
+        return Vec2.createVec2(builder, to.x, to.y);
 
-export const makeVec2 = (to: Point, builder: flatbuffers.Builder) => {
-
-    return Vec2.createVec2(builder, to.x, to.y);
+    }
 
 }
