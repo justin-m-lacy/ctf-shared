e4241ad441ac14177b51dd15163504e446314411
Justin Lacy <justin@launchbadge.com>
2022-08-24 19:27:55 -0700

    fix import/export paths.

--- src/messages/types.ts
+++ src/messages/types.ts
@@ -9,10 +9,10 @@ import { FlagDropped } from '../flatbuffers/ctf/flag-dropped';
 import { FlagReturned } from '../flatbuffers/ctf/flag-returned';
 import { FlagTaken } from '../flatbuffers/ctf/flag-taken';
 import { TeamScored } from '../flatbuffers/ctf/team-scored';
-import { ClientMove } from 'src/flatbuffers/ctf/client/client-move';
+import { ClientMove } from './src/flatbuffers/ctf/client/client-move';
 import { ClientChargeFire } from '../flatbuffers/ctf/client/client-charge-fire';
-import { ClientCancelFire } from 'src/flatbuffers/ctf/client/client-cancel-fire';
-import { ClientFire } from 'src/flatbuffers/ctf/client/client-fire';
+import { ClientCancelFire } from './src/flatbuffers/ctf/client/client-cancel-fire';
+import { ClientFire } from './src/flatbuffers/ctf/client/client-fire';
 
 export type CtfEvent = PlayerEvent | GameEvent;
 
